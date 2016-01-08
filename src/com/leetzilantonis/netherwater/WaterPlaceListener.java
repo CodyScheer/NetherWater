612d3a9c8d5d5cbcb45ef557eb750d9a8bb3c207
Lee Tzilantonis <LeePMC@users.noreply.github.com>
2016-01-09 10:12:15 +1100

    Updated to 1.0.1

--- src/com/leetzilantonis/netherwater/WaterPlaceListener.java
+++ src/com/leetzilantonis/netherwater/WaterPlaceListener.java
@@ -4,38 +4,30 @@ import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.World.Environment;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 
 public class WaterPlaceListener implements Listener {
 
-	private Main plugin;
-
-	public WaterPlaceListener(Main instance) {
-
-		plugin = instance;
-
-	}
-
+	@EventHandler (priority = EventPriority.MONITOR, ignoreCancelled = true)
 	public void onPlayerInteract(PlayerInteractEvent e) {
 
-		if (e.getAction() == Action.LEFT_CLICK_AIR
-				|| e.getAction() == Action.LEFT_CLICK_BLOCK
-				|| e.getAction() == Action.RIGHT_CLICK_AIR
-				|| e.isCancelled())
+		if (e.getAction() != Action.RIGHT_CLICK_BLOCK)
 			return;
 
 		World w = e.getClickedBlock().getWorld();
 		Player p = e.getPlayer();
 
-		if ((w.getEnvironment() == Environment.NETHER) && e.getItem().getType() == Material.WATER_BUCKET) {
+		if (w.getEnvironment() == Environment.NETHER && e.getItem().getType() == Material.WATER_BUCKET) {
 
 			if (p.hasPermission("netherwater.use." + w.getName()) || p.hasPermission("netherwater.use.*")) {
 
-				if (!plugin.dWorlds.contains(w.getName()) || p.hasPermission("netherwater.world.bypass")) {
+				if (!Main.getInstance().getWorlds().contains(w.getName()) || p.hasPermission("netherwater.world.bypass")) {
 
-					if (plugin.canBuild(p, e.getClickedBlock().getRelative(e.getBlockFace()))) {
+					if (Main.getInstance().canBuild(p, e.getClickedBlock().getRelative(e.getBlockFace()))) {
 						
 						e.setCancelled(true);
 						e.getClickedBlock().getRelative(e.getBlockFace()).setType(Material.WATER);
